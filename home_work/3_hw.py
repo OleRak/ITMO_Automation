e340d157cc038ace4fd5731c76a944806dce9a52
Oleg_Rakhimov <olegario.rakhimov@gmail.com>
2023-06-09 16:09:07 +0300

    hw_3

---
+++ home_work/3_hw.py
@@ -0,0 +1,39 @@
+a =3
+b =2
+
+if a > b:
+    print("MAX = ", (a))
+else:
+    print("MAX = ",(b))
+
+a = 235
+b = 101
+
+if (a - b) == 135 or (b - a) == 135:
+    print("yes")
+else:
+    print("no")
+
+a = 7
+if a >= 3 and a <= 5:
+    print("Весна")
+elif a>= 6 and a <= 8:
+    print("Лето")
+elif a >= 9 and a <= 11:
+    print("Осень")
+else:
+    print("Зима")
+
+a = 13
+b = 11
+c = 12
+if a >10 and b > 10 and c > 10:
+    print("yes")
+else:
+    print("no")
+
+a = [1, 2, -3, 4, 58]
+
+for num in a:
+    if num > 0:
+        print(len(num))
